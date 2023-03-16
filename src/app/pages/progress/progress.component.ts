0848ba283f4c7bca2ea10390145471a7ddaabbdb
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-16 03:17:43 +0100

    Added Charts

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styleUrls: ['./progress.component.css'],
 })
 export class ProgressComponent {
+  progress1: number = 20;
+  progress2: number = 25;
 
+  getProgress1() {
+    return `${this.progress1}%`;
+  }
+
+  getProgress2() {
+    return `${this.progress2}%`;
+  }
 }
