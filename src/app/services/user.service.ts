4e8bf0f6417aac2bb6653edd258be83c0849205a
eliezer.antonio <eliezer.antonio@ideiasdinamicas.com>
2023-04-21 23:39:55 +0100

    feat: implementation login method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,15 +3,43 @@ import { Injectable } from '@angular/core';
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { environment } from 'src/environments/environment.development';
 import { LoginForm } from '../interfaces/login-form..interface';
-import { tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+import { Router } from '@angular/router';
+import { User } from '../models/user.model';
 
 const base_url = environment.base_url;
+
+declare const google: any;
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) {}
+  public user!: User;
+  constructor(private http: HttpClient, private router: Router) {}
 
+  validateToken(): Observable<boolean> {
+    const token = localStorage.getItem('token') || '';
+
+    return this.http
+      .get(`${base_url}/login/renew`, {
+        headers: {
+          'x-token': token,
+        },
+      })
+      .pipe(
+        tap((resp: any) => {
+          const { email, google, name, role, img, uid } = resp.usuario;
+
+          console.log(resp.usuario);
+
+          this.user = new User(name, email, role, '', google, img, uid);
+
+          localStorage.setItem('token', resp.token);
+        }),
+        map((res) => true),
+        catchError((err) => of(false))
+      );
+  }
   createUser(formData: RegisterForm) {
     return this.http.post(`${base_url}/usuarios`, formData).pipe(
       tap((resp: any) => {
@@ -29,10 +57,17 @@ export class UserService {
   }
 
   loginGoogle(token: string) {
-    return this.http.post(`${base_url}/login/google`, token).pipe(
+    return this.http.post(`${base_url}/login/google`, { token }).pipe(
       tap((resp: any) => {
         localStorage.setItem('token', resp.token);
       })
     );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    google.accounts.id.revoke('eliezerfernandoantonio@gmail.com', () => {
+      this.router.navigateByUrl('/');
+    });
   }
 }
