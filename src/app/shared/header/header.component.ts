11b9391393fb7888004c483684ae49689808d682
eliezer.antonio <eliezer.antonio@ideiasdinamicas.com>
2023-04-21 23:38:55 +0100

    feat: get info user

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeaderComponent {
+  public user: User;
+  constructor(private userService: UserService) {
+    this.user = userService.user;
+  }
 
+  logout() {
+    this.userService.logout();
+  }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 import { SidebarService } from 'src/app/services/sidebar.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,9 +10,14 @@ import { SidebarService } from 'src/app/services/sidebar.service';
 })
 export class SidebarComponent {
   menuItems: any[];
+  public user: User;
 
-  constructor(private sideBarService: SidebarService) {
+  constructor(
+    private sideBarService: SidebarService,
+    userService: UserService
+  ) {
     this.menuItems = sideBarService.menu;
+    this.user = userService.user;
 
     console.log(this.menuItems);
   }
